3ebdd825e8213bda2df4a3b8fba2f7c151201f94
Mouni-Sanaboyina <mouni.31238@gmail.com>
2025-09-11 21:29:16 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import StudentDashboard from "./pages/StudentDashboard";
 import AttendancePage from "./pages/AttendancePage";
+import Interests.tsx from "./pages/Interests.tsx";
 import NotFound from "./pages/NotFound";
 
 const queryClient = new QueryClient();
@@ -20,6 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/student-dashboard" element={<StudentDashboard />} />
           <Route path="/attendance" element={<AttendancePage />} />
+          <Route path="/interests" element={<Interests />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
